3d9573c9d760e8d8d4a6f172676b6c9b274c1b30
belajar-vue-2 <aryaadi229@gmail.com>
2024-01-15 11:57:21 +0700

    feat:fix argvTime

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const notifier = require("node-notifier");
 const moment = require("moment");
 
 const argvTime = process.argv.slice(2);
-
 const working = argvTime[0];
 const breaking = argvTime[1];
 
-let workingStatus = false;
 let timer = 0;
+let workingStatus = false;
 
-function displayTimer(timer) {
-    const getMoment = moment.duration(timer);
+function displayTimer(time) {
+    const getMoment = moment.duration(time, "seconds");
     const hours = getMoment.hours().toString().padStart(2, "0");
     const minutes = getMoment.minutes().toString().padStart(2, "0");
     const seconds = getMoment.seconds().toString().padStart(2, "0");
@@ -18,25 +17,28 @@ function displayTimer(timer) {
     return `${hours} : ${minutes} : ${seconds}`;
 }
 
-function process(duration) {
+function startTimer(time) {
+    timer = time * 60;
     workingStatus = !workingStatus;
-    timer = duration * 60;
-
-    const timeStop = setInterval(() => {
+  
+    const interval = setInterval(() => {
         timer--;
 
         const display = displayTimer(timer);
-        console.log(`${workingStatus ? "Work" : "Break"} ${display}`)
+
+        console.log(`${workingStatus ? "Work" : "Break"} : ${display}`);
 
         if (timer === 0) {
-            clearInterval(timeStop);
+            clearInterval(interval);
             notifier.notify({
-                title: workingStatus ? "Working Time!" : "Breaking Time",
-                message: workingStatus ? "Good Break!" : "Good Work!",
+                title: workingStatus ? "Good Work!" : "Good Break!",
+                message: workingStatus ? "Break Time!" : "Work Time!",
             })
-            process(workingStatus ? working : breaking);
+            startTimer(workingStatus ? working : breaking);
         }
-    }, 1000)
+    }, 1000);
+
+
 }
 
-process(working);
+startTimer(working);
